d4e59ff7974cca804c4a77343abb4b81f52d43e7
WYeabkal <yeabkalwondimu411@gmail.com>
2022-10-18 18:15:25 +0300

    finished with mandatory tasks

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10x in a row
+ *
+ * Return: void.
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	for(int i = 0; i < 10; i++)
+	{
+		char yb = 'a';
+
+		while (yb <= 'z')
+		{
+			_putchar(yb);
+			yb++;
+		}
+
+		_putchar('\n');
+	}
+}
