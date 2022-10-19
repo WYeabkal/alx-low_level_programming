f138797f75a6e54c74b6a6765b63e68aebe50314
WYeabkal <yeabkalwondimu411@gmail.com>
2022-10-19 18:16:36 +0300

    finished with mandatory tasks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,23 +12,23 @@
 
 int print_sign(int n)
 {
-	int i = n;
+	int value;
 
 	if (i < 0)
 	{
-		return (-1);
+		value = -1;
 		_putchar ('-');
 	}
 	else if (i == 0)
 	{
-		return (0);
+		value = 0;
 		_putchar (0 + '0');
 	}
-	else if (i > 0)
+	else
 	{
-		return (1);
+		value = 1;
 		_putchar ('+');
 	}
 
-	return (0);
+	return (value);
 }
